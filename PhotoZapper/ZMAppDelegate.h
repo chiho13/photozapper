bca20eaa9f69fb690d97c6470ee2dab564422b05
Anthony Ho <ahomnky11@gmail.com>
2014-09-24 15:35:32 +0100

    Initial Commit

---
+++ PhotoZapper/ZMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZMAppDelegate.h
+//  PhotoZapper
+//
+//  Created by Anthony Ho on 24/09/2014.
+//  Copyright (c) 2014 ZeroMondays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoZapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoZapper
+//
+//  Created by Anthony Ho on 24/09/2014.
+//  Copyright (c) 2014 ZeroMondays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZMAppDelegate class]));
+    }
+}
